e21fd9029031aa6f6a894aec674e67b7857e3029
RodrDevs80 <rodr26707@gmail.com>
2024-11-22 18:51:02 -0300

    cambio rutas relativas img

--- src/data/contactos.js
+++ src/data/contactos.js
@@ -1,7 +1,7 @@
 export const contactos = [
-    { id: 1, nombre: "Ana", numero: "+5491123456789", img: "./src/assets/img-contactos/1.jpg" },
-    { id: 2, nombre: "Carlos", numero: "+5491123456790", img: "./src/assets/img-contactos/2.jpg" },
-    { id: 3, nombre: "Mariana", numero: "+5491123456791", img: "./src/assets/img-contactos/3.jpg" },
-    { id: 4, nombre: "Jose", numero: "+5491123456792", img: "./src/assets/img-contactos/4.jpg" },
-    { id: 5, nombre: "Laura", numero: "+5491123456793", img: "./src/assets/img-contactos/5.jpg" },
+    { id: 1, nombre: "Ana", numero: "+5491123456789", img: "../src/assets/img-contactos/1.jpg" },
+    { id: 2, nombre: "Carlos", numero: "+5491123456790", img: "../src/assets/img-contactos/2.jpg" },
+    { id: 3, nombre: "Mariana", numero: "+5491123456791", img: "../src/assets/img-contactos/3.jpg" },
+    { id: 4, nombre: "Jose", numero: "+5491123456792", img: "../src/assets/img-contactos/4.jpg" },
+    { id: 5, nombre: "Laura", numero: "+5491123456793", img: "../src/assets/img-contactos/5.jpg" },
 ]; 
