39686469e03857a567bfe7ce632e1513c2b316a6
RodrDevs80 <rodr26707@gmail.com>
2024-11-22 19:30:50 -0300

    test 1

--- src/data/contactos.js
+++ src/data/contactos.js
@@ -3,5 +3,5 @@ export const contactos = [
     { id: 2, nombre: "Carlos", numero: "+5491123456790", img: "..//src/assets/img-contactos/2.jpg" },
     { id: 3, nombre: "Mariana", numero: "+5491123456791", img: "..//src/assets/img-contactos/3.jpg" },
     { id: 4, nombre: "Jose", numero: "+5491123456792", img: "..//src/assets/img-contactos/4.jpg" },
-    { id: 5, nombre: "Laura", numero: "+5491123456793", img: "..//src/assets/img-contactos/5.jpg" },
+    { id: 5, nombre: "Laura", numero: "+5491123456793", img: "https://app-tipo-wp.netlify.app/.netlify/images?url=/5.jpg" },
 ]; 
